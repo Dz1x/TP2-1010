23205de986f3603b498168563998eb64cf044be6
Dz1x <36559220+Dz1x@users.noreply.github.com>
2019-05-19 00:28:28 -0400

    Update PolyLand.h

--- PolyLand.h
+++ PolyLand.h
@@ -16,28 +16,28 @@ public:
 
 	bool ajouterDresseur(const Dresseur& dresseur); // A MODIFIER... (si necessaire)
 	bool operator==(const Dresseur& dresseur) const;
-	PolyLand& operator+=(const Dresseur& dresseur) const;
-	
+	PolyLand operator+=(const Dresseur& dresseur);
+
 	bool ajouterCreature(const Creature& creature); // A MODIFIER... (si necessaire)
 	bool operator==(const Creature& creature) const;
-	PolyLand& operator+=(const Creature& creature) const;
+	PolyLand operator+=(const Creature& creature) ;
 
 	bool retirerDresseur(const string& nom); // A MODIFIER... (si necessaire)
-	PolyLand& operator-=(const Dresseur& dresseur) const;
+	PolyLand operator-=(const Dresseur& dresseur) ;
 
 	bool retirerCreature(const string& nom); // A MODIFIER... (si necessaire)
-	PolyLand& operator-=(const Creature& creature) const;
+	PolyLand operator-=(const Creature& creature);
 
 	Dresseur* choisirDresseurAleatoire(); // A MODIFIER... (si necessaire)
 	Creature* choisirCreatureAleatoire(); // A MODIFIER... (si necessaire)
 
 	bool attraperCreature(Dresseur* dresseur, const Creature& creature); // A MODIFIER... (si necessaire)
-	
+
 
 	bool relacherCreature(Dresseur* dresseur, const string& nomCreature); // A MODIFIER... (si necessaire)
-	
 
-	
+
+
 	friend ostream& operator<<(ostream& os, const PolyLand& polyLand); // A MODIFIER... (si necessaire)
 
 
