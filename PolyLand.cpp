0a6501c633d578a67143e112326ee15cf552408a
anassbahir <50391183+anassbahir@users.noreply.github.com>
2019-05-18 23:16:25 -0400

    Update PolyLand.cpp

--- PolyLand.cpp
+++ PolyLand.cpp
@@ -6,100 +6,141 @@
 
 PolyLand::PolyLand() // A MODIFIER... (si necessaire)
 {
-	dresseurs_ = new Dresseur*[MAX_DRESSEURS]();
-	creatures_ = new Creature*[MAX_CREATURES]();
+
 }
 
-
+// Is done
 PolyLand::~PolyLand() // A MODIFIER... (si necessaire)
 {
-	delete[]dresseurs_;
-	dresseurs_ = nullptr;
 
-	for (unsigned int i = 0; i < nombreCreatures_; i++)
+	for (Creature* creature : creatures_)
 	{
-		delete creatures_[i];
-		creatures_[i] = nullptr;
+		delete creature;
+		creature = nullptr;
 	}
-	delete[]creatures_;
-	creatures_ = nullptr;
+	for (Dresseur* dresseur : dresseurs_)
+	{
+		delete dresseur;
+		dresseur = nullptr;
+	}
 
 }
 
-bool PolyLand::ajouterDresseur(Dresseur* dresseur) // A MODIFIER... (si necessaire)
+// Is done
+bool PolyLand::ajouterDresseur(const Dresseur& dresseur) // A MODIFIER... (si necessaire)
 {
-	if (nombreDresseurs_ >= MAX_DRESSEURS)
-		return false;
-	for (unsigned int i = 0; i < nombreDresseurs_; i++)
+	for (Dresseur* dresseurDansVecteur : dresseurs_)
 	{
-		if (dresseurs_[i]->obtenirNom() == dresseur->obtenirNom())
+		if (dresseurDansVecteur->obtenirNom() == dresseur.obtenirNom())
 			return false;
 	}
-	dresseurs_[nombreDresseurs_++] = dresseur;
-	cout << dresseur->obtenirNom() << " a bien �t� ajout� !" << endl;
+	dresseurs_.push_back(new Dresseur(dresseur));
 	return true;
 }
 
+// is done
+bool PolyLand::operator==(const Dresseur& dresseur) const{ // À faire
+
+	for (Dresseur* dresseurDansVecteur : dresseurs_ ) {
+		if (dresseurDansVecteur->obtenirNom() == dresseur.obtenirNom()) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// À faire
+PolyLand& PolyLand::operator+=(const Dresseur& dresseur) const {
+
+}
+
+// Is done
 bool PolyLand::ajouterCreature(const Creature& creature) // A MODIFIER... (si necessaire)
 {
-	if(nombreCreatures_ >= MAX_CREATURES)
-		return false;
-	creatures_[nombreCreatures_] = new Creature();
-	*creatures_[nombreCreatures_] = creature;
-	nombreCreatures_++;
-	cout << creature.obtenirNom() << " a bien �t� ajout� !" << endl;
+	for (Creature* creatureDansVecteur : creatures_)
+	{
+		if (creatureDansVecteur->obtenirNom() == creature.obtenirNom())
+			return false;
+	}
+	creatures_.push_back(new Creature(creature));
 	return true;
 }
 
+// id done
+bool PolyLand::operator==(const Creature& creature) const { // À faire
+	for (Creature* creatureDansVecteur : creatures_) {
+		if (creatureDansVecteur->obtenirNom() == creature.obtenirNom()) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// À faire
+PolyLand& PolyLand::operator+=(const Creature& creature) const {
+	
+
+
+}
+
+// is done
 bool PolyLand::retirerDresseur(const string& nom) // A MODIFIER... (si necessaire)
 {
-	for (unsigned int i = 0; i < nombreDresseurs_; i++)
-	{
-		if (dresseurs_[i]->obtenirNom() == nom)
-		{
-			dresseurs_[i] = dresseurs_[nombreDresseurs_ - 1];
-			dresseurs_[nombreDresseurs_ - 1] = nullptr;
-			nombreDresseurs_--;
+	for (Dresseur* dresseurDansVecteur : dresseurs_) {
+		if (dresseurDansVecteur->obtenirNom == nom) {
+			dresseurDansVecteur = dresseurs_[dresseurs_.size() - 1];
+			dresseurs_.pop_back;
 			return true;
 		}
 	}
 	return false;
 }
 
+// À faire
+PolyLand& PolyLand::operator-=(const Dresseur& dresseur) const {
+
+}
+
+// is done
 bool PolyLand::retirerCreature(const string& nom) // A MODIFIER... (si necessaire)
 {
-	for (unsigned int i = 0; i < nombreCreatures_; i++)
+	for (Creature* creatureDansVecteur : creatures_)
 	{
-		if (creatures_[i]->obtenirNom() == nom)
-		{
-			delete creatures_[i];
-			creatures_[i] = creatures_[nombreCreatures_ - 1];
-			creatures_[nombreCreatures_ - 1] = nullptr;
-			nombreCreatures_--;
+		if (creatureDansVecteur->obtenirNom == nom){
+
+			creatureDansVecteur = creatures_[creatures_.size() - 1];
+			creatures_.pop_back;
 			return true;
 		}
 	}
 	return false;
 }
 
+// À faire
+PolyLand& PolyLand::operator-=(const Creature& creature) const {
+
+}
+
+//  is done
 Dresseur* PolyLand::choisirDresseurAleatoire() // A MODIFIER... (si necessaire)
 {
-	if (nombreDresseurs_ > 0) 
-    {
-		unsigned int indice = rand() % nombreDresseurs_;
+	if (!dresseurs_.empty())
+	{
+		unsigned int indice = rand() % dresseurs_.size();
 		return dresseurs_[indice];
 	}
-	else 
-    {
+	else
+	{
 		return nullptr;
 	}
 }
 
+// is done
 Creature* PolyLand::choisirCreatureAleatoire() // A MODIFIER... (si necessaire)
 {
-	if (nombreCreatures_ > 0) 
-    {
-		unsigned int indice = rand() % nombreCreatures_;
+	if (!creatures_.empty())
+	{
+		unsigned int indice = rand() % creatures_.size();
 		return creatures_[indice];
 	}
 	else {
@@ -107,7 +148,8 @@ Creature* PolyLand::choisirCreatureAleatoire() // A MODIFIER... (si necessaire)
 	}
 }
 
-bool PolyLand::attraperCreature(Dresseur* dresseur,const Creature& creature) // A MODIFIER... (si necessaire)
+
+bool PolyLand::attraperCreature(Dresseur* dresseur, const Creature& creature) // A MODIFIER... (si necessaire)
 {
 	return dresseur->ajouterCreature(creature);
 }
@@ -117,29 +159,21 @@ bool PolyLand::relacherCreature(Dresseur* dresseur, const string& nomCreature)
 	return dresseur->enleverCreature(nomCreature);
 }
 
-void PolyLand::infoDresseur(const string& nom) const // A MODIFIER... (si necessaire)
+
+ostream& operator<<(ostream& os, const PolyLand& polyLand) // A MODIFIER... (si necessaire)
 {
-	Dresseur* dresseur = nullptr;
-	for (unsigned int i = 0; i < nombreDresseurs_; i++)
-	{
-		if (dresseurs_[i]->obtenirNom() == nom)
-		{
-			dresseur = dresseurs_[i];
-			break;
-		}
+	// A finir de là
+	cout << "INFORMATION SUR POLYLAND" << endl;
+	for (Dresseur* dresseur: polyLand.dresseurs_) {
+		cout << dresseur;
 	}
-	if (dresseur != nullptr)
-	{
-		cout << "Informations sur le dresseur: " << endl;
-		dresseur->affichage();
-		for (unsigned int i = 0; i < dresseur->obtenirNombreCreatures(); i++)
-		{
-			cout << "- " << i + 1 << " - ";
-			dresseur->obtenirCreatures()[i]->information();
-		}
+
+	cout << endl;
+
+	for (Creature* creature : polyLand.creatures_) {
+		cout << creature;
 	}
-	else
-	{
-		cout << "Dresseur introuvable!" << endl;
-	}
+
+	// Le return est bon
+	return os;
 }
