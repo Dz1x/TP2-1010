b42bc2804e8b78d3a6bb5e84cd9ef0a89977fca4
anassbahir <50391183+anassbahir@users.noreply.github.com>
2019-05-18 23:16:54 -0400

    Update PolyLand.h

--- PolyLand.h
+++ PolyLand.h
@@ -13,25 +13,40 @@ class PolyLand
 public:
 	PolyLand(); // A MODIFIER... (si necessaire)
 	~PolyLand(); // A MODIFIER... (si necessaire)
+
+	bool ajouterDresseur(const Dresseur& dresseur); // A MODIFIER... (si necessaire)
+	bool operator==(const Dresseur& dresseur) const;
+	PolyLand& operator+=(const Dresseur& dresseur) const;
 	
-	bool ajouterDresseur(Dresseur* dresseur); // A MODIFIER... (si necessaire)
 	bool ajouterCreature(const Creature& creature); // A MODIFIER... (si necessaire)
+	bool operator==(const Creature& creature) const;
+	PolyLand& operator+=(const Creature& creature) const;
 
 	bool retirerDresseur(const string& nom); // A MODIFIER... (si necessaire)
+	PolyLand& operator-=(const Dresseur& dresseur) const;
+
 	bool retirerCreature(const string& nom); // A MODIFIER... (si necessaire)
+	PolyLand& operator-=(const Creature& creature) const;
 
 	Dresseur* choisirDresseurAleatoire(); // A MODIFIER... (si necessaire)
 	Creature* choisirCreatureAleatoire(); // A MODIFIER... (si necessaire)
 
 	bool attraperCreature(Dresseur* dresseur, const Creature& creature); // A MODIFIER... (si necessaire)
+	
+
 	bool relacherCreature(Dresseur* dresseur, const string& nomCreature); // A MODIFIER... (si necessaire)
 	
-	void infoDresseur(const string& nom) const; // A MODIFIER... (si necessaire)
+
+	
+	friend ostream& operator<<(ostream& os, const PolyLand& polyLand); // A MODIFIER... (si necessaire)
+
 
 	//________TP2_______
 
 private:
 	//________TP2_______
+	vector<Dresseur*> dresseurs_;
+	vector<Creature*> creatures_;
 
 };
 #endif
