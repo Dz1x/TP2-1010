a7747fb19071c04021a47f00aafd98e1a3f63d85
anassbahir <50391183+anassbahir@users.noreply.github.com>
2019-05-20 04:18:42 -0400

    Update Dresseur.h

--- Dresseur.h
+++ Dresseur.h
@@ -9,39 +9,43 @@
 class Dresseur
 {
 public:
+	// ___TP1____
 	Dresseur(); // A MODIFIER... (si necessaire)
 	Dresseur(const string& nom,const string&equipe); // A MODIFIER... (si necessaire)
 	~Dresseur(); // A MODIFIER... (si necessaire)
-	Creature* obtenirUneCreature(const string&nom) const;
+
 	string obtenirNom() const;
-	string obtenirNomEquipe() const;
-	void modifierNom(const string& nom);
-	
+	Creature* obtenirUneCreature(const string&nom) const;
+	ObjetMagique obtenirObjetMagique() const;
 	unsigned int obtenirNombreCreatures() const; // A MODIFIER... (si necessaire)
 
-	vector<Creature*> obtenirCreatures() const; // A MODIFIER... (si necessaire)
+	void modifierNom(const string& nom);
 	void modifierCreature(vector<Creature*> creatures); // A MODIFIER... (si necessaire)
-	
-	ObjetMagique obtenirObjetMagique() const;
 	void modifierObjetMagique(const ObjetMagique& objetMagique);
-
-	void utiliserObjetMagique(Creature* creature);
 	
-	bool ajouterCreature(const Creature& creature); // A MODIFIER... (si necessaire)
+	bool ajouterCreature( Creature& creature); // A MODIFIER... (si necessaire)
 	bool enleverCreature(const string& nom); // A MODIFIER... (si necessaire)
+	
+	void utiliserObjetMagique(Creature* creature);
+
+	// ___TP2____
+	vector<Creature*> obtenirCreatures() const; // A MODIFIER... (si necessaire)
+	string obtenirNomEquipe() const;
+	
 	bool operator==(const Dresseur&dresseur) const;
 	bool operator==(const string&nom)const;
 	friend bool operator==(const string&nom,const Dresseur&dresseur);
+	
 	friend ostream&operator<<(ostream&os,const Dresseur&dresseur); // A COMPLETER... (si necessaire)
 
 private:
+	// ___TP1____
 	string nom_;
 	ObjetMagique objetMagique_;
 
 	// ___TP2____
 	vector<Creature*> creatures_;
 	string equipe_;
-
 };
 
 #endif
